a9d3780187462e0d275fb955fddac776a6797ec6
dharun <dharunpalanisamy5256@gmail.com>
2025-03-14 21:26:18 +0530

    leetcode endpoints

--- Utils/LeetcodeUtils.js
+++ Utils/LeetcodeUtils.js
@@ -7,12 +7,6 @@ const getProfileData = async(username) => {
         query: `
             query userPublicProfile($username: String!) {
               matchedUser(username: $username) {
-                contestBadge {
-                  name
-                  expired
-                  hoverText
-                  icon
-                }
                 username
                 profile {
                   ranking
@@ -33,17 +27,198 @@ const getProfileData = async(username) => {
                 body: JSON.stringify(query)
             }
         );
-        console.log('res camse\n')
         const data = await response.json();
         if(data.errors!=null) {
-            data.errors.forEach(element => {
-                console.log(element.message)
-            });
+            return {
+                status: 404,
+                message: data.errors[0].message
+            }
         }
-        else console.log(data.data.matchedUser)
+        else {
+            return {
+                status : 200,
+                message: "Profile fetched sucessfully",
+                data : data.data.matchedUser
+            }
+        }
     }catch(error){
-        console.log('Error : ',error);
+        return {
+            status : 500,
+            message: "Internal Server Error",
+            error : error
+        }
     }
 }
 
-getProfileData('DharunAP');
+const getProblemSolved = async(username) => {
+    const query = {
+        query:`
+            query userProblemsSolved($username: String!) {
+            allQuestionsCount {
+                difficulty
+                count
+            }
+            matchedUser(username: $username) {
+                problemsSolvedBeatsStats {
+                difficulty
+                percentage
+                }
+                submitStatsGlobal {
+                acSubmissionNum {
+                    difficulty
+                    count
+                }
+                }
+            }
+            }
+        `,
+        variables: { username }
+    };
+    try{
+        const response = await fetch(apiUrl,{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(query)
+        });
+        const data = await response.json();
+        return {
+            status : 200,
+            message: "Data fetched sucessfully",
+            data : data.data
+        }
+    }
+    catch(error){
+        return {
+            status : 500,
+            message: "Internal server error"
+        }
+    }
+}
+
+const getLangStats = async(username) => {
+    const query = {
+        query: `
+            query languageStats($username: String!) {
+                matchedUser(username: $username) {
+                    languageProblemCount {
+                    languageName
+                    problemsSolved
+                    }
+                }
+            }
+        `,
+        variables : {username}
+    };
+    try{
+        const response = await fetch(apiUrl, {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(query)
+        });
+        const data = await response.json();
+        return {
+            status : 200,
+            message: 'Data fetched sucessfully',
+            data : data.data.matchedUser
+        }
+    }catch(error){
+        return {
+            status : 500,
+            message: 'Internal server error'
+        }
+    }
+}
+
+const getSkillStats = async(username) => {
+    const query = {
+        query : `
+        query skillStats($username: String!) {
+            matchedUser(username: $username) {
+                tagProblemCounts {
+                advanced {
+                    tagName
+                    tagSlug
+                    problemsSolved
+                }
+                intermediate {
+                    tagName
+                    tagSlug
+                    problemsSolved
+                }
+                fundamental {
+                    tagName
+                    tagSlug
+                    problemsSolved
+                }
+                }
+            }
+            }
+        `,
+        variables : { username }
+    };
+    try{
+        const response = await fetch(apiUrl,{
+            method : 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body : JSON.stringify(query)
+        });
+        const data = await response.json();
+        return {
+            status : 200,
+            message: 'Data fetched sucessfully',
+            data : data.data.matchedUser
+        }
+    }catch(error){
+        return {
+            status : 500,
+            message: 'Internal server error'
+        }
+    }
+}
+
+/*
+(async()=>{
+    const values = await getSkillStats('DharunAP');
+    console.log(values.data)
+})();
+*/
+
+/*
+(async () => {
+    let values = await getLangStats('DharunAP');
+    console.log(values.data)
+})();
+*/
+
+/*
+(async () => {
+    let values = await getProblemSolved('DharunAP');
+    console.log(values)
+})();
+*/
+
+/*
+(async () => {
+    let values = await getProfileData('DharunAP');
+    console.log(values);
+    // {
+    //     status: 200,
+    //     message: 'Profile fetched sucessfully',
+    //     data: {
+    //       username: 'DharunAP',
+    //       profile: {
+    //         ranking: 86718,
+    //         userAvatar: 'https://assets.leetcode.com/users/DharunAP/avatar_1732074291.png',
+    //         realName: 'Dharun A P',
+    //         skillTags: [Array]
+    //       }
+    //     }
+    //   }
+})();
+*/
